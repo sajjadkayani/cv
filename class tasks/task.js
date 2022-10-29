79e136ddfb764730ac45b9a0aaa98a75ad5b3207
sajjad <sajjad.kayaniiii313@gmail.com>
2022-10-29 00:16:55 -0700

    new folder

---
+++ class tasks/task.js
@@ -0,0 +1,42 @@
+// function task(a) {
+//   var result ="";
+//   for (let i = 1 ; i<arguments.length ; i++){
+//     result += arguments[i] + a;
+//     return result;
+//   }
+// }
+// console.log(task("a,a,a,abababadad,ada,ada"));
+// function task(a , ...args) {
+  
+  
+//     return args.length + 1 ;
+// }
+//     console.log(task("a,a,a,abababadad,ada,ada")+ "arguments");
+    
+// function sumNUms (...nums){
+//     let sum =0 ;
+//     for(let i = 0 ; i < nums.length ; i++){
+//         sum += nums[i];
+//         return sum;
+//     }
+// }
+// const abc = function (a) {
+//     return a * a;
+// }
+// var b=abc(5)
+// console.log(b)
+    
+    
+const abc = (a)=> {
+    return a * b;
+    
+    function bcd() {
+        var b=25
+    }
+   
+} 
+var b=abc(5)
+
+console.log(abc)
+    
+    
